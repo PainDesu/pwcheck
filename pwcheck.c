80b48a79c67815a964a9962df6b33ef9325750b7
Alexandr Vrána <43996537+PainDesu@users.noreply.github.com>
2021-10-31 15:59:41 +0100

    Comments + bugfixes

--- pwcheck.c
+++ pwcheck.c
@@ -22,24 +22,42 @@ struct params{
     bool stats;
 };
 
-//Takes string and returns it's length
-int getLength(char* str){
+/* Function: get_length
+*  ------------------------
+*   Calculates the lenght of a string
+*
+*   str*: string that gets checked
+*
+*   returns: Length of a string
+*/
+int get_length(char* str){
     int ans = 0;
     for (int i = 0; str[i] !='\0'; i++)
     {
         ans++;
     }
+    if(str[ans] == '\n'){
+        return ans - 1;
+    }
     return ans;
 }
 
-//Compares two strings, returns true if they are same
-bool mystrComp(char* str1, char* str2){
+/* Function: my_str_comp
+*  ------------------------
+*   Compares two strings if they are the same
+*
+*   str1*: First compared string
+*   str2*: Second compared string
+*
+*   returns: True if the strings are the same
+*/
+bool my_str_comp(char* str1, char* str2){
     int size;
-    if(getLength(str1) != getLength(str2)){
+    if(get_length(str1) != get_length(str2)){
         return false;
     }
     else{
-        size = getLength(str1);
+        size = get_length(str1);
     }
     for(int i = 0; i <= size; i++){
         if(str1[i] != str2[i]){
@@ -75,8 +93,15 @@ bool capital_check(struct password pass){
     return (small && capital);
 }
 
-//Returns true if passowrd contains special symbol
-bool checkSpecialChar(struct password pass){
+/* Function: check_special_char
+*  ------------------------
+*   Checks if password contains special chars
+*
+*   pass: struct password that gets checked
+*
+*   returns: True if special char is used
+*/
+bool check_special_char(struct password pass){
     for (int i = 0; i <= pass.length; i++)
     {
         if ((pass.string[i] >= 33 && pass.string[i] <= 47) || (pass.string[i] >= 58 && pass.string[i] <= 64) || (pass.string[i] >= 91 && pass.string[i] <= 96) || (pass.string[i] >= 123 && pass.string[i] <= 126))
@@ -88,7 +113,14 @@ bool checkSpecialChar(struct password pass){
     return false;
 }
 
-//Returns true if password contains number
+/* Function: check_num
+*  ------------------------
+*   Checks if password contains number
+*
+*   pass: struct password that gets checked
+*
+*   returns: True if number is used
+*/
 bool check_num(struct password pass){
     for(int i = 0; i <= pass.length; i++){
         if((pass.string[i] >= '0') && (pass.string[i] <= '9')){
@@ -98,12 +130,20 @@ bool check_num(struct password pass){
     return false;
 }
 
-//Returns true if 2nd level of checks works
+/* Function: groups_check
+*  ------------------------
+*   Checks if password contains number of groups of different possible chars
+*
+*   pass: struct password that gets checked
+*   param: number of groups that need to be checked
+*
+*   returns: True if the number of groups is higher or equal to param
+*/
 bool groups_check(struct password pass, int param){
     if(capital_check(pass) == false){
         return false;
     };
-    int count = 2 + checkSpecialChar(pass) + check_num(pass);
+    int count = 2 + check_special_char(pass) + check_num(pass);
     int checkNum = param;
     
     if(checkNum > 4){
@@ -116,7 +156,15 @@ bool groups_check(struct password pass, int param){
     return false;
 }
 
-//Returns size of longest repeating chars
+/* Function: longest_repeat_check
+*  ------------------------
+*   Checks if password contains substring of same repeating char longer then param
+*
+*   pass: struct password that gets checked
+*   param: lenght of substring that is not allowed
+*
+*   returns: True if there are no substrings of same repeating char longer then param
+*/
 bool longest_repeat_check(struct password pass, int param){
     if(groups_check(pass, param) == false){
         return false;
@@ -135,6 +183,15 @@ bool longest_repeat_check(struct password pass, int param){
     return true;
 }
 
+/* Function: substring_check
+*  ------------------------
+*   Checks if password contains substrings of size param
+*
+*   pass: struct password that gets checked
+*   param: lenght of substrings that is not allowed
+*
+*   returns: True if no 2 substrings of lenght param are found in pass
+*/
 bool substrings_check(struct password pass, int param){
     if(longest_repeat_check(pass, param) == false){
         return false;
@@ -162,6 +219,15 @@ bool substrings_check(struct password pass, int param){
     return true;
 }
 
+/* Function: set_params
+*  ------------------------
+*   Takes arguments and puts them into struct params
+*
+*   argv[][]: Arguments that get transfered
+*   parameters*: Pointer to struct params that get updated
+*
+*   returns: True if arguments are valid and got transfered into parameters
+*/
 bool set_params(char* argv[], struct params* parameters){
     int buffer = strtol(argv[0], NULL, 10);
     if(buffer == 0){
@@ -184,12 +250,14 @@ bool set_params(char* argv[], struct params* parameters){
     return true;
 }
 
-/*  Checks password and adds values into stats
-    Parameters:
-    struct password == Current password that gets added to stats
-    struct stats* == Pointer to stats struct, that gets updated
+/* Function: stats_count
+*  ------------------------
+*   Adds current password into the stats
+*
+*   pass: struct password that gets added into the counts
+*   stats*: pointer to the current stats struct that keeps the statistics
 */
-void statsCount (struct password pass, struct stats* stats){
+void stats_count (struct password pass, struct stats* stats){
     if(pass.length < stats->shortestPass){
         stats->shortestPass = pass.length;
     }
@@ -200,8 +268,19 @@ void statsCount (struct password pass, struct stats* stats){
     }
 }
 
+/* Function: final_stats_count
+*  ------------------------
+*   Does last calculation of stats before printing
+*
+*   stats*: pointer to the current stats struct that keeps the statistics
+*/
 void final_stats_count(struct stats* stats){
-    stats->avg = stats->charCount / stats->passCount;
+    if(stats->passCount == 0){
+        stats->avg = 0;
+    }
+    else{
+        stats->avg = stats->charCount / stats->passCount;
+    }
     for(int i = 0; i <= 93; i++){
         stats->uniqueCount = stats->uniqueCount + stats->uniqueChars[i];
     }
@@ -218,7 +297,7 @@ int main(int argc, char* argv[])
     bool valid;
     if(argc >= 3 && argc <= 4){
         if(argc == 4){
-            if(mystrComp(argv[3], "--stats")){
+            if(my_str_comp(argv[3], "--stats")){
                 parameters.stats = true;
             }
             else{
@@ -233,11 +312,10 @@ int main(int argc, char* argv[])
             return EXIT_FAILURE;
         }
     }
-
-    
+   
     while(fgets(pass.string, 102, stdin) != NULL){
-        pass.length = getLength(pass.string) - 1; //-1 for '\n'
-        statsCount(pass, stats);
+        pass.length = get_length(pass.string);
+        stats_count(pass, stats);
         valid = false;
         switch (parameters.level)
         {
@@ -262,7 +340,8 @@ int main(int argc, char* argv[])
         }
     }
         if(parameters.stats){
-            fprintf(stdout,"Statistika: \n Ruznych znaku: %d \n Minimalni delka: %d \n Prumerna delka: %.1f \n", statistics.charCount, statistics.charCount, statistics.passCount/1.0f );
+            final_stats_count(stats);
+            fprintf(stdout,"Statistika: \n Ruznych znaku: %d \n Minimalni delka: %d \n Prumerna delka: %.1f \n", statistics.uniqueCount, statistics.shortestPass, statistics.avg );
         }
     return EXIT_SUCCESS;
 }
